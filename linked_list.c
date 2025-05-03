b8660649141c4cf7a2348f44e9bc2a7d23929f73
Yoshita Mahesh <ymahesh@eceprog2.ecn.purdue.edu>
2025-05-02 21:25:23 -0400

    done

--- linked_list.c
+++ linked_list.c
@@ -2,17 +2,78 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) {
+  struct list_node * newNode = calloc(1, sizeof(struct list_node));
+   newNode -> value = value;
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+   return newNode;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) {
+  struct list_node *newNode = new_node(value);
+  newNode -> next = list -> head;
+  list -> head = newNode;
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+void insert_at_tail(struct linked_list *list, size_t value) {
+  struct list_node *last = list -> head;
+  if(last == NULL) {
+    last = new_node(value);
+    return;
+  }
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+  while (last -> next != NULL) {
+    last = last -> next;
+  }
 
-void free_list(struct linked_list list) {}
+  last -> next = new_node(value);
+}
+
+size_t remove_from_head(struct linked_list *list) { 
+  struct list_node * toDelete = list -> head;
+  if (list -> head == NULL) {
+    return 0;
+  }
+  
+  
+  size_t val = list -> head -> value;
+  list -> head = list-> head -> next;
+  free(toDelete);
+
+  return val; 
+}
+
+size_t remove_from_tail(struct linked_list *list) {
+struct list_node * toDelete = list -> head;
+struct list_node * prev = NULL;
+
+if (list -> head == NULL) {
+  return 0;
+}
+
+if (toDelete -> next == NULL) {
+  return remove_from_head(list);
+}
+
+while (toDelete -> next != NULL) {
+  prev = toDelete;
+  toDelete = toDelete -> next;
+}
+
+size_t val = toDelete -> value;
+prev -> next = NULL;
+free(toDelete);
+
+return val;
+}
+
+void free_list(struct linked_list list) {
+
+  while (list.head != NULL) {
+    remove_from_head(&list);
+  }
+
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
--- queue.c
+++ queue.c
@@ -1,8 +1,104 @@
 #include "queue.h"
 #include "tile_game.h"
+#include "linked_list.h"
 
-void enqueue(struct queue *q, struct game_state state) {}
+void enqueue(struct queue *q, struct game_state state) {
+    insert_at_head(&q -> data, serialize(state));
+}
 
-struct game_state dequeue(struct queue *q) { return (struct game_state){0}; }
+struct game_state dequeue(struct queue *q) { 
+    return deserialize(remove_from_tail(&q -> data)); 
+}
 
-int number_of_moves(struct game_state start) { return 0; }
+//function to check end_state and current state
+bool check_end(struct game_state current, struct game_state end_state) {
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            if(current.tiles[i][j] != end_state.tiles[i][j]) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+//function to store visited states
+bool check_visited(struct game_state current, struct linked_list *visited) {
+    current.num_steps = 0;
+    size_t current_val = serialize(current);
+
+    struct list_node *n = visited-> head;
+    while (n != NULL) {
+        if(current_val == n -> value) {
+            return 0;
+        }
+        n = n->next;
+    }
+    return 1;
+
+}
+
+int number_of_moves(struct game_state start) { 
+    struct queue queue;
+    struct linked_list visited = { .head = NULL };
+    queue.data.head = NULL;
+
+    struct game_state end_state = {
+        .tiles = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15}},
+        .empty_row = 3, 
+        .empty_col = 3, 
+        .num_steps = 0
+    };
+
+    enqueue(&queue, start);
+    while(queue.data.head != NULL) {
+        struct game_state cur = dequeue(&queue);
+        if(check_end(cur, end_state)) { // check how to set end_state
+            free_list(queue.data);
+            free_list(visited);
+            return cur.num_steps;
+        }
+        if(cur.empty_row != 3) { //check move up
+            struct game_state copy = cur;
+            move_up(&copy);
+
+            if(check_visited(copy, &visited)) {
+                enqueue(&queue, copy);
+                copy.num_steps = 0;
+                insert_at_head(&visited, serialize(copy)); 
+            }
+        }
+        if(cur.empty_row != 0) { //check move down
+            struct game_state copy = cur;
+            move_down(&copy);
+
+            if(check_visited(copy, &visited)) {
+                enqueue(&queue, copy); 
+                copy.num_steps = 0;
+                insert_at_head(&visited, serialize(copy));
+            }
+        }
+        if(cur.empty_col != 0) { //check move right
+            struct game_state copy = cur;
+            move_right(&copy);
+
+            if(check_visited(copy, &visited)) {
+                enqueue(&queue, copy); 
+                copy.num_steps = 0;
+                insert_at_head(&visited, serialize(copy));
+            }
+        }
+        if(cur.empty_col != 3) { //check move left
+            struct game_state copy = cur;
+            move_left(&copy);
+
+            if(check_visited(copy, &visited)) {
+                enqueue(&queue, copy); 
+                copy.num_steps = 0;
+                insert_at_head(&visited, serialize(copy));
+            }
+        }
+    }
+    return -1;
+}
+
